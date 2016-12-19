455ae93bb097a7e817dd1752fa632823a895c2db
Chandi Charan Mishra <Chandi Charan Mishra>
2016-12-20 00:30:51 +0530

    initial commit

---
+++ MavenTest/src/main/java/com.tcs/Test.java
@@ -0,0 +1,13 @@
+package com.tcs;
+
+/**
+ * Created by chand on 12/17/2016.
+ */
+public class Test {
+    public static  void main(String args[]){
+
+        System.out.println("Hello world!");
+
+    }
+
+}
